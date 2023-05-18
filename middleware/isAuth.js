4218b60ba3f0d52117e2889eb744d2f16f83862c
manishkhulbe123@gmail.com <manishkhulbe123@gmail.com>
2023-05-19 00:13:43 +0530

    isAuth updated

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -1,29 +1,3 @@
-// const ObjectId = require("mongodb").ObjectId;
-// const JwtStrategy = require("passport-jwt").Strategy
-// const ExtractJwt = require("passport-jwt").ExtractJwt
-// const passport = require("passport");
-// var opts = {
-//   jwtFromRequest: ExtractJwt.fromHeader("authorization"),
-//   secretOrKey: process.env.JWT_SECRET,
-//   passReqToCallback: true,
-// };
-
-// const jwtLogin = new JwtStrategy(opts, async (req, payload, done) => {
-//   let getUser = await User.findOne({ _id: new ObjectId(payload._id) });
-//   if (getUser) {
-//     return done(null, user);
-//   }
-//   const error = new Error("Please login");
-//   error.status = 401;
-//   return done(error, false);
-// });
-
-// passport.use(jwtLogin);
-
-// module.exports = {
-//   isAuth : passport.authenticate("jwt", { session: false }),
-// };
-
 let JwtStrategy = require("passport-jwt").Strategy;
 let ExtractJwt = require("passport-jwt").ExtractJwt;
 const { ObjectId } = require("mongodb");

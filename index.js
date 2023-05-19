06df28a30daeea80b8330cb355c59c8b6b986c02
manishkhulbe123@gmail.com <manishkhulbe123@gmail.com>
2023-05-19 17:42:32 +0530

    index.js updated

--- index.js
+++ index.js
@@ -7,16 +7,18 @@ const {dbConnection} = require("./db/index");
 dbConnection.connect().then((db) => {
   const app = express();
   app.use(express.json());
-  
-  app.options('/*', (_, res) => {
-    res.sendStatus(200);
-  });
+
+  //cors
   app.use((request, response, next) => {
     response.header("Access-Control-Allow-Origin", "*");
     response.header('Access-Control-Allow-Credentials', true);
     response.header("Access-Control-Allow-Headers", "*");
     response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
+  });
+
+  app.options('/*', (req, res) => {
+    res.sendStatus(200);
   });
 
   //routes
